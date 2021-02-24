b01ccb6c32c530aa5765307d7ebbf752052de9b0
p1zza <38226114+p1zza@users.noreply.github.com>
2021-02-24 17:52:10 +0300

    Add files via upload

--- Practics.py
+++ Practics.py
@@ -5,9 +5,6 @@ print ('Давай "Знакомиться"?')
 name = input('Как тебя зовут? \n')
 print('Привет,', name, '! \n')
 
-
-'''
-'''
 #0002
 print(dir())
 # dir - список доступных переменных в программе
@@ -35,6 +32,37 @@ print(2**3) #степень
 print('2aaaaaaaa a' * 3) #вывести любое значение несколько раз
 a = '4**5 + 2'
 print(eval(a)) #eval парсит строку и выполняет её 
-''''
+'''
+'''
+#0005
+print(bin(25))
+print(oct(25))
+print(hex(25))
+print(bin(5))
+print(bin(~5))
 
+'''
+'''
+#0006
+print(' ' + bin(250))
+print(bin(~250))
+print('x + (-x)=0')
+print('x + (~x) = 1111...111 + 1')
+print('x + (~x) + 1 = 1111...111 + 1')
+print('x + (~x) + 1 = 0')
+print('~x = -x - 1')
+print('~x = ~x + 1')
+print('x = -(~x = 1)')
+print('x = -(~x + 1)')
+print(bin(~(-250)))
+print('~a == -a -1')
+'''
 
+#0007
+print(bin(50) + '\n' + bin(30))
+print(' ' + bin(50&30))
+print(bin(50&30))
+print(bin(50|30))
+print(bin(50^30))
+print(bin(50<<3))
+print(bin(50>>3))
